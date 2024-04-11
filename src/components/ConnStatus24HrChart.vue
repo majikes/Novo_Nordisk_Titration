562daeaf389e7241ec53f425d9b7eb57be425f2c
qqp5np <qqp5np@virginia.edu>
2024-04-11 13:23:20 -0400

    fighting d3 and apparently winning

--- src/components/ConnStatus24HrChart.vue
+++ src/components/ConnStatus24HrChart.vue
@@ -1,203 +1,152 @@
 <template>
-  <div class="tir-chart" ref="tirContainer">
-    <svg class="svg-demo" :viewBox="viewBox">
-      <g class="series">
-        <rect v-for="(d, i) in visibleBarData" :key="i" :x="xScale('TiR')" :y="yScale(startHeights[i])" :ry="radius"
-          :height="yScale(d - barGap)" :width="xScale.bandwidth()" :fill="colorScale(i.toString())" />
-      </g>
+  <div id="cgmAvailabilityContainer" ref="resizeRef">
+    <svg ref="svgRef" height="60">
+      <g class="x-axis" />
     </svg>
   </div>
 </template>
 <script setup lang="ts">
 
-import { computed, defineProps, onMounted, PropType, ref } from 'vue';
-import { useDebugModeStore } from "@/stores/debugModeStore";
-import { type CGMDataAvail5MinDiasType } from "@/types/CGMDataAvailTypes";
-import * as d3 from 'd3'
+//  https://dev.to/muratkemaldar/using-vue-3-with-d3-composition-api-3h1g
+// https://github.com/muratkemaldar/using-vue3-with-d3
+// big ol' props to that person
+
+import { computed, defineProps, onMounted, PropType, ref, watchEffect } from 'vue';
+import { type CGMDataAvail5MinDiasType } from "@/types/SubjectCardTypes";
+import {
+  select,
+  line,
+  scaleLinear,
+  scaleTime,
+  min,
+  max,
+  curveBasis,
+  axisBottom,
+  axisLeft,
+  timeHour,
+} from 'd3'
+import useResizeObserver from "@/use/resizeObserver";
+
 
 
 const props = defineProps({
   connData: {
     required: true,
-    type: Boolean as PropType<boolean>
+    type: Array as PropType<CGMDataAvail5MinDiasType[]>
   },
-  loaded: {
+  loading: {
     required: true,
     type: Boolean as PropType<boolean>
   },
-  height: {
-    required: true,
-    type: Number as PropType<number>
-  },
-  width: {
-    required: true,
-    type: Number as PropType<number>
-  },
-  radius: {
-    required: true,
-    type: Number as PropType<number>
-  },
-  colors: {
-    required: false,
-    type: Array as PropType<string[]>
-  },
+  // height: {
+  //   required: true,
+  //   type: Number as PropType<number>
+  // },
+  // width: {
+  //   required: true,
+  //   type: Number as PropType<number>
+  // },
 })
 
-// this never changes, so it doesn't need ref()
-// const padding = 60
-// console.log(props.tirheight, typeof(props.tirheight))
+// create ref to pass to D3 for DOM manipulation
+const svgRef = ref(null);
 
-const debugModeStore = useDebugModeStore()
-const tirContainer = ref({} as HTMLElement)
+// this creates another ref to observe resizing, 
+// which we will attach to a DIV,
+// since observing SVGs with the ResizeObserver API doesn't work properly
+const { resizeRef, resizeState } = useResizeObserver();
 
-interface TiRData {
-  tir: string,
-  lt54: number,
-  bt5470: number,
-  bt70180: number,
-  bt180250: number,
-  gt250: number,
-}
-
-const data = computed(() => {
-  const tirBar = { tir: "TiR" } as TiRData
-  // map values
-  tirBar.lt54 = props.tirData.lt54
-  tirBar.bt5470 = props.tirData.bt5470
-  tirBar.bt70180 = props.tirData.bt70180
-  tirBar.bt180250 = props.tirData.bt180250
-  tirBar.gt250 = props.tirData.gt250
-  return [tirBar]
-})
-
-// let barGap = ref(2)
-const barGap = computed(() => {
-  let barGap = 2
-  if (typeof (props.gap) !== 'undefined') {
-    barGap = props.gap
+// custom computed values that represent just arrays of data points
+// all times
+const multiVector = computed(() => {
+  const retObj = {
+    timeVec: [] as number[],
+    egvExistsVec: [] as number[],
+    egvCountVec: [] as number[],
+    egvMultipleVec: [] as boolean[],
   }
-  return barGap
-})
-
-const minH = computed(() => {
-  let tmpMin = props.radius + barGap.value
-  if (typeof (props.minHeight) !== 'undefined') {
-    tmpMin = props.minHeight
+  for (const elem of props.connData) {
+    retObj.timeVec.push(elem.baseTime)
+    retObj.egvExistsVec.push(elem.egvPresent ? 1 : 0)
+    retObj.egvCountVec.push(elem.egvCount)
+    retObj.egvMultipleVec.push(elem.egvMultiple)
   }
-  return tmpMin
+  return retObj
 })
-
-const visibleBarData = computed(() => {
-  const lt54 = props.tirData.lt54 > 0 ? Math.max(props.tirData.lt54, minH.value) : 0
-  const bt5470 = props.tirData.bt5470 > 0 ? Math.max(props.tirData.bt5470, minH.value) : 0
-  const bt180250 = props.tirData.bt180250 > 0 ? Math.max(props.tirData.bt180250, minH.value) : 0
-  const gt250 = props.tirData.gt250 > 0 ? Math.max(props.tirData.gt250, minH.value) : 0
-  const bt70180 = props.tirData.bt70180 > 0 ? 100 - (lt54 + bt5470 + bt180250 + gt250) : 0
-  const ret = {
-    lt54: lt54,
-    bt5470: bt5470,
-    bt70180: bt70180,
-    bt180250: bt180250,
-    gt250: gt250,
-  }
-  console.log('fixed tir vals', ret)
-  return ret
-})
-
-const startHeights = computed(() => {
-  return {
-    lt54: visibleBarData.value.gt250 + visibleBarData.value.bt180250 + visibleBarData.value.bt70180 + visibleBarData.value.bt5470,
-    bt5470: visibleBarData.value.gt250 + visibleBarData.value.bt180250 + visibleBarData.value.bt70180,
-    bt70180: visibleBarData.value.gt250 + visibleBarData.value.bt180250,
-    bt180250: visibleBarData.value.gt250,
-    gt250: 0,
-  }
-})
-
-// const dataArr = computed(() => {
-//   return [props.tirData.lt54, props.tirData.bt5470, props.tirData.bt70180, props.tirData.bt180250, props.tirData.gt250]
-// })
-
-const padding = ref(0)
-
-// these two are updated on mounted()
-const height = ref(props.tirheight)
-const width = ref(props.tirwidth)
-
-// const radius = ref(props.radius)
-
-const rangeX = computed(() => {
-  const w = width.value - padding.value
-  return [0, w]
-})
-const rangeY = computed(() => {
-  const h = height.value - padding.value
-  return [0, h]
-})
-
-const xScale = d3.scaleBand()
-  .domain(["TiR"])
-  .range(rangeX.value)
-  .padding(0.1);
-
-const yScale = d3.scaleLinear()
-  .domain([0, 100])
-  .range(rangeY.value);
-
-// const colorScale = computed(() => d3.scaleOrdinal().range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"]))
-const colorList = computed(() => {
-  // custom color support here based on existence of color prop
-  const defaults = ["#ab3025", "#d59792", "#44c973", "#f9c63d", "#f5a000"]
-  let retArr = []
-  if (typeof (props.colors) !== 'undefined') {
-    retArr = props.colors
-  } else {
-    retArr = defaults
-  }
-  return retArr
-})
-const colorScale = computed(() => d3.scaleOrdinal().range(colorList.value))
-
-// const computedData = computed(() => d3.stack()(dataArr.value.map((d) => ({ value: d }))).pop())
-
-const viewBox = computed(() => { return `0 0 ${width.value} ${height.value}` })
-
-// const heights = computed(() => {
-//   const heightRet = {
-//     'vhigh': 0,
-//     'high': 0,
-//     'target': 0,
-//     'low': 0,
-//     'vlow': 0,
-//   }
-//   let validVals = 0
-//   for (const val in props.tirData) {
-//     if ((props.tirData as any)[val] > 0) {
-//       validVals += 1
-//     }
-//   }
-//   const gaps = validVals - 1
-//   // each gap is ~5px so we're working w/ height minus 5*gaps
-//   const realHeight = height.value - (gaps * 5)
-//   heightRet.vhigh = Math.round(realHeight * (props.tirData.gt250 / 100))
-//   heightRet.high = Math.round(realHeight * (props.tirData.bt180250 / 100))
-//   // heightRet.target = realHeight * (props.tirData.bt70180/100)
-//   heightRet.low = Math.round(realHeight * (props.tirData.bt5470 / 100))
-//   heightRet.vlow = Math.round(realHeight * (props.tirData.lt54 / 100))
-
-//   return heightRet
-// })
 
 onMounted(() => {
-  // tirContainer.value = 
-  if (tirContainer.value instanceof HTMLElement) {
-    height.value = tirContainer.value.clientHeight
-    width.value = tirContainer.value.clientWidth
-  }
-  console.log('tir mounted')
-  // console.log(height.value)
-})
+  // pass ref with DOM element to D3, when mounted (DOM available)
+  const svg = select(svgRef.value);
+
+  // whenever any dependencies (like data, resizeState) change, call this!
+  watchEffect(() => {
+    const { width, height } = resizeState.dimensions;
+    // console.log(`detected width x height change: ${width} x ${height}`)
+
+    // scales: map index / data values to pixel values on x-axis / y-axis
+    const xScale = scaleTime()
+      // .domain([0, props.data.length - 1]) // input values...
+      // just setting domain to 288-1, cause we're only looking at 1 day
+      .domain([
+        new Date((min(multiVector.value.timeVec) as number)*1000), 
+        new Date((max(multiVector.value.timeVec) as number)*1000)
+      ]) // input values...
+      .range([0, width]) // ... output values
+      .nice();
+    
+    const xMargin = 20;
+
+    const yScale = scaleLinear()
+      .domain([0,1]) // input values...
+      .range([height, 0]); // ... output values
+
+    // // line generator: D3 method to transform an array of values to data points ("d") for a path element
+    // const lineGen = line()
+    //   .curve(curveBasis)
+    //   .x((value, index) => xScale(index))
+    //   .y((value) => yScale(value));
+
+    // // render path element with D3's General Update Pattern
+    // svg
+    //   .selectAll(".line") // get all "existing" lines in svg
+    //   .data([props.data]) // sync them with our data
+    //   .join("path") // create a new "path" for new pieces of data (if needed)
+
+    //   // everything after .join() is applied to every "new" and "existing" element
+    //   .attr("class", "line") // attach class (important for updating)
+    //   .attr("stroke", "green") // styling
+    //   .attr("d", lineGen); // shape and form of our line!
+
+    const xAxis = axisBottom(xScale)
+      .ticks(timeHour.every(6));
+    
+    const gx = svg.select(".x-axis")
+
+    if (xAxis) {
+      gx
+      .attr("transform", `translate(0,${height-xMargin})`)
+      .call(xAxis)
+    }
+    
+    // svg.append("g")
+      
+    
+    
+    // const yAxis = axisLeft(yScale);
+    // svg.append("g")
+    //   .call(yAxis)
+
+    // render axes with help of scales
+    // (we let Vue render our axis-containers and let D3 populate the elements inside it)
+    
+    // svg
+    //   .select(".x-axis")
+    //   .style("transform", `translateY(${height}px)`) // position on the bottom
+    //   .call(xAxis);
+
+    
+    // svg.select(".y-axis").call(yAxis);
+  });
+});
 
 </script>
-
-<style scoped></style>
--- src/components/SubjectCard.vue
+++ src/components/SubjectCard.vue
@@ -3,7 +3,8 @@
     :class="{ 'bg-white text-gray-400': !isActive, 'bg-emerald-100': isActive }">
     <div class="py-1">{{ card.subject_id }}</div>
     <div class="px-2 py-1 rounded-md"
-      :class="{ 'bg-cyan-500': armColor.ctrColor, 'bg-fuchsia-500': armColor.expColor }">
+      :class="{ 'bg-cyan-500': armColor.ctrColor, 'bg-fuchsia-500': armColor.expColor }"
+      :title="armTitle">
       {{ armText }}
     </div>
 
@@ -12,7 +13,7 @@
       <div>Status: {{ status }}</div>
     </div>
     <div class="basis-full flex justify-between relative" id="cgm-availability">
-      <div>Conn: Loading...</div>
+      <ConnStatus24HrChart :conn-data="card.cgm_availability" :loading="card.loading" />
     </div>
 
   </div>
@@ -20,6 +21,7 @@
 <script setup lang="ts">
 import { computed, defineProps, PropType, ref } from "vue";
 import { type SubjectCardFrontendType } from "@/types/SubjectCardTypes";
+import ConnStatus24HrChart from "./ConnStatus24HrChart.vue";
 
 const props = defineProps({
   card: {
@@ -45,6 +47,11 @@ const armColor = computed(() => {
     }
   }
   return retObj
+})
+const armTitle = computed(() => {
+  if (props.card.interventionArm === 0) { return 'CONTROL ARM' }
+  else if (props.card.interventionArm === 1) { return 'EXPERIMENTAL ARM' }
+  else { return 'N/A' }
 })
 const armText = computed(() => {
   if (props.card.interventionArm === 0) { return 'CTR' }
